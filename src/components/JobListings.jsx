72869893f406ddbc7353139c30e4255b3306b6d2
Sara Ahmad Malik <sarahahmadmalik55@gmail.com>
2024-06-22 16:01:27 +0500

    fix(server-error):removed

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -8,10 +8,11 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs';
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
+        console.log(data)
         setJobs(data);
       } catch (error) {
         console.log('Error fetching data', error);
