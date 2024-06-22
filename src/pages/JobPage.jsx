6f1e011e183d1eb015e6f068a7513e3f65734406
Sara Ahmad Malik <112407915+sarahahmadmalik@users.noreply.github.com>
2024-06-22 19:51:41 +0500

    Update JobPage.jsx

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,34 +1,33 @@
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import {useState, useEffect} from 'react';
 
 const JobPage = ({ deleteJob }) => {
+  
   const navigate = useNavigate();
   const { id } = useParams();
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
+  const [job, setJob] = useState();
+  
+useEffect(() => {
     const fetchJob = async () => {
       try {
         setLoading(true);
-        const response = await fetch(`https://jobs-server-api-ashy.vercel.app/api/jobs/${id}`);
+        const response = await fetch(`/api/jobs/${id}`);
         if (!response.ok) {
           throw new Error('Failed to fetch job');
         }
         const data = await response.json();
         setJob(data);
-        setLoading(false);
       } catch (error) {
-        setError(error.message);
-        setLoading(false);
+        console.log(error)
       }
     };
 
     fetchJob();
   }, [id]);
+  
 
   const onDeleteClick = (jobId) => {
     const confirm = window.confirm(
@@ -133,4 +132,10 @@ const JobPage = ({ deleteJob }) => {
   );
 };
 
-export default JobPage;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = await res.json();
+  return data;
+};
+
+export { JobPage as default, jobLoader };
